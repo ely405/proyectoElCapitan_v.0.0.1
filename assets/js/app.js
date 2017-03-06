141dd26c258ceac1f904847453cb90ec52c52624
ely211 <gonzalesm405@gmail.com>
2017-03-06 18:29:55 -0500

    dropdown

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,6 +9,24 @@ function printTemas(divtexte) {
 
   if(divtexte == sprint1){
     document.getElementById("sprint2").style.display = "none";
-    //document.getElementById("sprint3").style.display = "none";
+
   }
-  }
+}
+
+function myFunction() {
+	document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+	if (!event.target.matches('.dropbtn')) {
+
+		var dropdowns = document.getElementsByClassName("dropdown-content");
+		var i;
+		for (i = 0; i < dropdowns.length; i++) {
+			var openDropdown = dropdowns[i];
+			if (openDropdown.classList.contains('show')) {
+				openDropdown.classList.remove('show');
+			}
+		}
+	}
+}
