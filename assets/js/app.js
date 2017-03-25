7780750b51e0c55f789b1bac86929c420885a807
ely211 <gonzalesm405@gmail.com>
2017-03-25 08:09:13 -0500

    validación quiz sprint1

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,50 +1,52 @@
 function Capitan(){
-  this.countChecked = 0;
-  this.recordText = [];
-  //this.options = document.getElementsByName(name);
 
   this.printTemas = function(numSprint){
-      var sprint1 = document.getElementById("divSprint1");
-      var sprint2 = document.getElementById("divSprint2");
-      var sprint3 = document.getElementById("divSprint3");
+    var sprint1 = document.getElementById("divSprint1");
+    var sprint2 = document.getElementById("divSprint2");
+    var sprint3 = document.getElementById("divSprint3");
 
-      if(numSprint == "divSprint1"){
-        sprint1.style.display = "block";
-        sprint2.style.display = "none";
-        sprint3.style.display = "none";
-        alert("hola");
-      }else if(numSprint == "divSprint2"){
-        sprint1.style.display = "none";
-        sprint2.style.display = "block";
-        sprint3.style.display = "none";
-        alert("hola2");
-      }else if(numSprint == "divSprint3"){
-        sprint1.style.display = "none";
-        sprint2.style.display = "none";
-        sprint3.style.display = "block";
-        alert("hola3");
-      }
+    if(numSprint == "divSprint1"){
+      sprint1.style.display = "block";
+      sprint2.style.display = "none";
+      sprint3.style.display = "none";
+      alert("sprint1");
+    }else if(numSprint == "divSprint2"){
+      sprint1.style.display = "none";
+      sprint2.style.display = "block";
+      sprint3.style.display = "none";
+      alert("sprint2");
+    }else if(numSprint == "divSprint3"){
+      sprint1.style.display = "none";
+      sprint2.style.display = "none";
+      sprint3.style.display = "block";
+      alert("sprint3");
     }
+  }
 
-  this.quizSolution = function(name, idSpan){
-    var options = document.getElementsByName(name);
-    var chooseOption = document.getElementById(idSpan);
-  //  var record = document.getElementById(idSpanResultado);
-    var optionSelected = false;
-    for(var i = 0; i <options.length; i++){
+  this.quizSolution = function(classNameInput, classNameSpan, idQuizContainer){
+    var options = document.getElementsByClassName(classNameInput);
+    var countChecked = 0;
+    var countCorrectOption = 0;
+    //var form1 = document.forms[0];
+    //var errorMessage = document.getElementsByName(classNameSpan);
+    var record = document.getElementById(idQuizContainer);
+    for(var i = 0; i< options.length; i++){
       if(options[i].checked){
-        //this.countChecked++;
-        optionSelected = true;
-        //this.recordText.push(options[i].value);
-        console.log(optionSelected);
-        console.log(this.recordText);
-        //record.innerHTML = this.recordText;
-        chooseOption.innerHTML = "";
-        break;
-      }else{
-        chooseOption.innerHTML = "escoge una opcion";
+        countChecked++;
+        console.log(countChecked);
       }
     }
+    if(countChecked == 3){
+      alert("tres");
+      (options[0].checked)? (countCorrectOption++):"";
+      (options[4].checked)? countCorrectOption++:"";
+      (options[8].checked)? countCorrectOption++:"";
+      console.log(countCorrectOption);
+      record.innerHTML = "Otbuviste " + countCorrectOption + " respuestas correctas.";
+    }else{
+      alert("Todas las preguntas som obligtorias");
+    }
+
   }
 
   this.solutionQuiz1 = function(name, iCorrect){
@@ -73,30 +75,25 @@ window.addEventListener("load", function(){
 
   document.getElementById("enviaQuiz1").addEventListener("click", function(event){
     event.preventDefault();
-    elCapitan.quizSolution("quiz1-opcA", "spnSelectOptionQ1-A");
-    elCapitan.quizSolution("quiz1-opcB", "spnSelectOptionQ1-B");
-    elCapitan.quizSolution("quiz1-opcC", "spnSelectOptionQ1-C");
-    elCapitan.solutionQuiz1("quiz1-opcA", 1);
-    //console.log(elCapitan.options);
+    elCapitan.quizSolution("inpQuiz1", "error", "pregQuiz1");
   });
-console.log(elCapitan.options);
 });
 
 
 function myFunction() {
-	document.getElementById("myDropdown").classList.toggle("show");
+  document.getElementById("myDropdown").classList.toggle("show");
 }
 
 window.onclick = function(event) {
-	if (!event.target.matches('.dropbtn')) {
+  if (!event.target.matches('.dropbtn')) {
 
-		var dropdowns = document.getElementsByClassName("dropdown-content");
-		var i;
-		for (i = 0; i < dropdowns.length; i++) {
-			var openDropdown = dropdowns[i];
-			if (openDropdown.classList.contains('show')) {
-				openDropdown.classList.remove('show');
-			}
-		}
-	}
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
 }
